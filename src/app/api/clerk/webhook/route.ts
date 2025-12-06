0e3cb03356fa96117d714256743a044529e3aa06
Tripathi <aditya.bietj@gmail.com>
2025-12-07 00:53:37 +0530

    fixed webhook

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -3,7 +3,7 @@ import { db } from "@/server/db"
 export const POST  = async (req: Request) => {
     const{ data }= await req.json()
     console.log('clerk webhook received', data)
-    const emailAddress = data.email_addresses[0].email_addresse
+    const emailAddress = data.email_addresses[0].email_address
     const firstName = data.first_name
     const lastName = data.last_name
     const ImgUrl  = data.image_url;
