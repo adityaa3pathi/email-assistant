248ae690c43328f445d66e67e3ae811a7ac52893
Tripathi <aditya.bietj@gmail.com>
2025-12-06 23:42:14 +0530

    now webhook will update the db with new user

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,6 +1,24 @@
+import { db } from "@/server/db"
+
 export const POST  = async (req: Request) => {
     const{ data }= await req.json()
     console.log('clerk webhook received', data)
+    const emailAddress = data.email_addresses[0].email_addresse
+    const firstName = data.first_name
+    const lastName = data.last_name
+    const ImgUrl  = data.image_url;
+
+    const id = data.id
+    await db.user.create({
+        data: {
+            id: id,
+            emailAddress: emailAddress,
+            firstName: firstName,
+            lastName: lastName,
+            imageUrl: ImgUrl,
+            
+        }
+    })
 
     return new Response('webhook received', {status: 200})
 }
